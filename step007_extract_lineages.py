2bce92e89b512a665a74eb6f1e9137d2fa03504a
Erick Peirson <erick.peirson@asu.edu>
2016-07-25 14:31:52 -0400

    ack

--- step007_extract_lineages.py
+++ step007_extract_lineages.py
@@ -23,16 +23,21 @@ def extract_lineage(root):
     lineage.append((thisTaxId, thisName, thisRank))
     return lineage
 
+
 if __name__ == '__main__':
     TAXONOMY_BASE = '/Users/erickpeirson/modelorganisms/ncbi/taxonomy'
     GRAPH_PATH = '/Users/erickpeirson/modelorganisms/ncbi/taxonomy.graphml'
+    REBASE_PATH = '/Users/erickpeirson/modelorganisms/ncbi/taxonomy_rebase.graphml'
     ONTOGRAPH_PATH = '/Users/erickpeirson/modelorganisms/ncbi/ontotaxonomy.graphml'
     graph = nx.DiGraph()
+    graph_rebase = nx.DiGraph()
     ontograph = nx.DiGraph()
     for fname in os.listdir(TAXONOMY_BASE):
         print '\r', fname,
         if not fname.endswith('xml'):
             continue
+
+        taxon_id = fname.split('.')[0]
 
         lineage = extract_lineage(ET.parse(os.path.join(TAXONOMY_BASE, fname)))
         graph.add_edge((-1, 'Root', 'Root'), lineage[0])
@@ -40,7 +45,13 @@ if __name__ == '__main__':
             if i == len(lineage) - 1:
                 break
             graph.add_edge(node, lineage[i+1])
+            graph_rebase.add_edge(str(node[0]), str(lineage[i+1][0]))
             ontograph.add_edge(node[2], lineage[i+1][2])
 
+        if str(lineage[-1][0]) != taxon_id:
+            graph.add_edge(node, (int(taxon_id), lineage[-1][1], lineage[-1][2]))
+            graph_rebase.add_edge(str(lineage[-2][0]), taxon_id)
+
     nx.write_graphml(graph, GRAPH_PATH)
+    nx.write_graphml(graph_rebase, REBASE_PATH)
     nx.write_graphml(ontograph, ONTOGRAPH_PATH)
