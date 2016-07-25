179cfe9dd9380fe729d471339f8870cf43f05551
Erick Peirson <erick.peirson@asu.edu>
2016-07-25 09:44:26 -0400

    bugfix

--- step004b_scrape_funding.py
+++ step004b_scrape_funding.py
@@ -15,41 +15,51 @@ if __name__ == '__main__':
     with open(MESH_TERMS, 'r') as f:
         terms = [line.strip() for line in f.readlines() if len(line) > 1]
 
-    df = pd.DataFrame(columns=['PMID', 'Year', 'Term', 'GrantID', 'Acronym', 'Agency', 'Country'])
-    found = 0.
+    df = pd.DataFrame(columns=['PMID', 'Year', 'Term', 'GrantID',
+                               'Acronym', 'Agency', 'Country'])
+
+    found = 0.    # Monitoring.
     tried = 0.
     for term in terms:
         for year in xrange(START_YEAR, END_YEAR):
-
-            sys.stdout.flush()
+            sys.stdout.flush()    # Monitoring.
             ty_dirpath = os.path.join(DATAPATH, term, str(year))
             if not os.path.exists(ty_dirpath):
+                # Some term-year combinations yielded no records.
                 print '\r skipping', term, year, ' (no PubMed response)',
                 continue
 
             for fname in os.listdir(ty_dirpath):
-                tried += 1.
+                tried += 1.    # For monitoring only.
 
-                if not fname.endswith('xml'):
+                if not fname.endswith('xml'):    # Skip hidden/unrelated files.
                     continue
 
                 pmid = fname.split('.')[0]
 
-                r = ET.parse(build_path(term, year, fname, DATAPATH, make=False)).getroot()
+                # PubMed XML records are already on disk, separated into
+                #  separate files for each record.
+                rec_path = build_path(term, year, fname, DATAPATH, make=False)
+                r = ET.parse(rec_path).getroot()
+
+                # Only around 20% of records actually have grant information.
+                #  This improves as we move forward to more recent publications.
                 grantlist = r.find('.//GrantList')
-                if not grantlist:
+                if grantlist is None:
                     continue
 
+                # There can be several grants per publication.
                 for grant in grantlist.findall('.//Grant'):
+                    # Grant records vary in their level of completeness.
                     grant_id = getattr(grant.find('.//GrantID'), 'text', None)
                     acronym = getattr(grant.find('.//Acronym'), 'text', None)
                     agency = getattr(grant.find('.//Agency'), 'text', None)
                     country = getattr(grant.find('.//Country'), 'text', None)
 
+                    # New row in the dataframe.
                     i = df.shape[0]
-                    df[i] = [pmid, year, term, grant_id, acronym, agency, country]
+                    df.loc[i] = [pmid, year, term, grant_id, acronym, agency, country]
                     found += 1.
 
-
-                print '\r', term, year, fname, found/tried
+            print '\r', term, year, fname, found/tried
     df.to_csv(OPATH, sep='\t')
