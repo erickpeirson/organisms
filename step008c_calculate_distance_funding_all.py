273696af8b32c92aa190fae61ea784f7917af3f9
Erick Peirson <erick.peirson@asu.edu>
2016-07-30 13:34:08 -0400

    calc

--- step008c_calculate_distance_funding_all.py
+++ step008c_calculate_distance_funding_all.py
@@ -5,52 +5,73 @@ from multiprocessing import Pool
 import cPickle as pickle
 import time
 import sys
-
-
-def calculate_diversity(df, term, year, nih):
-    import networkx as nx
-    from calc import dist_value
-    from util import parse_ner_hit
-    from itertools import combinations
-
-    calculated = {}
-
-    samples = []
-    if df.shape[0] > 0:
-        for i_full, j_full in combinations(df['#entity'].values, 2):
-            i = parse_ner_hit(i_full)
-            j = parse_ner_hit(j_full)
-
-            value = calculated.get((i, j), None)
-            if value is None:
-                value = dist_value(i, j)
-                calculated[(i, j)] = value
-            samples.append((i, j, value))
-    _save_result((term, year, samples, nih))
-    return term, year, samples, nih
+import networkx as nx
+from calc import dist_value
+from util import parse_ner_hit
+from itertools import combinations, islice
+import multiprocessing as mp
 
 
 def _save_result(result):
     RESULTS_BASE = '/Users/erickpeirson/modelorganisms/ncbi/diversity_raw_funding'
-    term, year, samples, nih = result
-    result_path = os.path.join(RESULTS_BASE, '%s_%i_%s.pickle' % (term, year, nih))
+    term, year, nih, m, v, s = result
+    result_path = os.path.join(RESULTS_BASE, '' % (term, year, nih))
     with open(result_path, 'w') as f:
         pickle.dump(result, f)
     return
 
 
+def calculate_diversity(df, chunk_size=5000):
+    print term, year, nih
+    if df.shape[0] == 0:
+        return 0.
+
+    subsets = []
+    value_counts = df['#entity'].value_counts()
+    combos = combinations(izip(value_counts.keys(), value_counts.values), 2)
+
+    while True:
+        result = pool.map(calculate_diversity_chunk, islice(combos, chunk_size))
+        if result:
+            subsets.extend(result)
+        else:
+            break
+    sums, counts = zip(*subsets)
+    return np.sum(sums)/np.sum(counts)
+
+
+def calculate_diversity_chunk(iterator):
+    print '.',
+    from calc import dist_value
+    from util import parse_ner_hit
+    import numpy as np
+
+    diversity = np.array([])
+    for (i_full, N_i), (j_full, N_j) in iterator:
+        i = parse_ner_hit(i_full)
+        j = parse_ner_hit(j_full)
+
+        N = N_i * N_j
+        d = dist_value(i, j)
+        diversity = np.concatenate((diversity, np.repeat(d, N)))
+    return diversity.sum(), diversity.shape[0]
+
+
 if __name__ == '__main__':
     NER_BASE = '/Users/erickpeirson/modelorganisms/ncbi/ner'
     FUNDING_BASE = '/Users/erickpeirson/modelorganisms/ncbi/funding/'
+    RESULTS_BASE = '/Users/erickpeirson/modelorganisms/ncbi/diversity_raw_funding'
+
 
     MESH_TERMS = 'mesh_diseases.txt'
     START_YEAR = 1975   # Starting in this year.
     END_YEAR = 2016    # Up to but not including this year.
-    # p = Pool(16)
+    pool = mp.Pool(processes=16)
 
     with open(MESH_TERMS, 'r') as f:
         terms = [line.strip() for line in f.readlines() if len(line) > 1]
 
+    df_results = pd.DataFrame(columns=['term', 'year', 'mean', 'nih'])
     jobs = []
     for year in xrange(START_YEAR, END_YEAR):
         df_combined_nih = pd.DataFrame()
@@ -69,7 +90,6 @@ if __name__ == '__main__':
             pmid_is_nih = np.vectorize(lambda v: v in nih_pmids)
             pmid_is_not_nih = np.vectorize(lambda v: v not in nih_pmids)
 
-
             df_year = df_term[df_term.year == year]
             if df_year.shape[0] == 0:
                 continue
@@ -86,6 +106,12 @@ if __name__ == '__main__':
                 df_combined_nih = df_combined_nih.append(df_year_nih)
             df_combined_not_nih = df_combined_not_nih.append(df_year_not_nih)
 
+        d_nih = calculate_diversity(df_combined_nih, chunk_size=5000)
+        d_notnih = calculate_diversity(df_combined_nih, chunk_size=5000)
+        idx = d_nih.shape[0]
+        df_results.loc[idx] = ('all', year, d_nih, 'nih')
+        idx = d_nih.shape[0]
+        df_results.loc[idx] = ('all', year, d_notnih, 'notnih')
 
-        apply(calculate_diversity, (df_combined_nih, 'all', year, 'nih'))
-        apply(calculate_diversity, (df_combined_not_nih, 'all', year, 'not_nih'))
+        df_combined_nih.to_csv(os.path.join(RESULTS_BASE, 'all_%i_nih.csv' % year), sep='\t')
+        df_combined_not_nih.to_csv(os.path.join(RESULTS_BASE, 'all_%i_notnih.csv' % year), sep='\t')
